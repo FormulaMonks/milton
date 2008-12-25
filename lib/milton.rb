d97bab5bacbedce7ee93fe27b045f51958e5d31c
Ben Alavi <benalavi@gmail.com>
2008-12-25 14:40:28 -0800

    documented attachment.rb and milton.rb, removed some unused methods

--- lib/milton.rb
+++ lib/milton.rb
@@ -5,6 +5,9 @@ require 'milton/is_uploadable'
 
 module Citrusbyte
   module Milton
+    # Raised when a file which was expected to exist appears not to exist
+    class MissingFileError < StandardError;end;
+
     # Some definitions for file semantics used throughout Milton, understanding
     # this will make understanding the code a bit easier and avoid ambiguity:
     #
@@ -18,37 +21,40 @@ module Citrusbyte
     # dirname:
     #   the directory portion of the path to a file or directory, all the chars
     #   up to the final /
-    #     /var/log/apache2 => /var/log/apache2
+    #     /var/log/apache2            => /var/log
+    #     /var/log/apache2/           => /var/log/apache2
     #     /var/log/apache2/access.log => /var/log/apache2
     #
     # basename:
     #   the portion of a filename *with no extension* (ruby's "basename" may or
     #   may not have an extension), all the chars after the last / and before
     #   the last .
-    #     /var/log/apache2 => nil
-    #     /var/log/apache2/access.log => access
+    #     /var/log/apache2                 => apache2
+    #     /var/log/apache2/                => nil
+    #     /var/log/apache2/access.log      => access
     #     /var/log/apache2/access.2008.log => access.2008
     #
     # extension:
     #   the extension portion of a filename w/ no preceding ., all the chars
     #   after the final .
-    #     /var/log/apache2 => nil
-    #     /var/log/apache2/access.log => log
+    #     /var/log/apache2                 => nil
+    #     /var/log/apache2/                => nil
+    #     /var/log/apache2/access.log      => log
     #     /var/log/apache2/access.2008.log => log
     # 
     # filename:
     #   the filename portion of a path w/ extension, all the chars after the
     #   final /
-    #     /var/log/apache2 => nil
-    #     /var/log/apache2/access.log => access.log
+    #     /var/log/apache2                 => apache2
+    #     /var/log/apache2/                => nil
+    #     /var/log/apache2/access.log      => access.log
     #     /var/log/apache2/access.2008.log => access.2008.log
     #   can also be defined as:
-    #     filename == basename + '.' + extension
+    #     filename == basename + (extension ? '.' + extension : '')
     #
     
     def self.included(base)
       base.extend Citrusbyte::Milton::BaseMethods
-      base.extend Citrusbyte::Milton::ClassMethods
       base.extend Citrusbyte::Milton::IsUploadable::IsMethods
       base.extend Citrusbyte::Milton::IsResizeable::IsMethods
       base.extend Citrusbyte::Milton::IsImage::IsMethods
@@ -56,6 +62,9 @@ module Citrusbyte
     
     module BaseMethods
       protected
+        # The attachment methods give the core of Milton's file-handling, so
+        # various extensions can use this when they're included to make sure
+        # that the core attachment methods are available
         def ensure_attachment_methods(options={})
           unless included_modules.include?(Citrusbyte::Milton::Attachment)
             include Citrusbyte::Milton::Attachment
@@ -63,13 +72,5 @@ module Citrusbyte
           end
         end
     end
-    
-    module ClassMethods
-      def random_string(length)
-         Digest::SHA1.hexdigest((1..6).collect { (i = rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }.join).slice(1..length)
-      end
-    end
-    
-    class MissingFileError < StandardError;end;
   end
 end
--- lib/milton/attachment.rb
+++ lib/milton/attachment.rb
@@ -17,7 +17,12 @@ module Citrusbyte
           # replacement
           options[:separator]        ||= '.'
           options[:replacement]      ||= '-'
+          
+          # root of where the underlying files are stored (or will be stored)
+          # on the file system
           options[:file_system_path] ||= File.join(RAILS_ROOT, "public", table_name)
+          
+          # mode to set on stored files and created directories
           options[:chmod]            ||= 0755
           
           AttachableFile.options = options
@@ -26,36 +31,47 @@ module Citrusbyte
           
           before_destroy :destroy_attached_file
           
-          extend  Citrusbyte::Milton::Attachment::ClassMethods
           include Citrusbyte::Milton::Attachment::InstanceMethods
         end
-      end
-      
-      module ClassMethods
       end
       
       module InstanceMethods
         # Sets the filename to the given filename (sanitizes the given filename
         # as well)
+        #
+        # TODO: change the filename on the underlying file system on save so ass
+        # not to orphan the file
         def filename=(name)
           write_attribute :filename, AttachableFile.sanitize_filename(name)
         end
 
+        # The path the file, takes an optional has of options which can be used
+        # to determine a particular derivative of the file desired
         def path(options={})
           attached_file.path(options)
         end
                 
         protected
+          # A reference to the attached file, this is probably what you want to
+          # overwrite to introduce a new behavior
+          #
+          # i.e. 
+          #   have attached_file return a ResizeableFile, or a TranscodableFile
           def attached_file
             @attached_file ||= AttachableFile.new(self, filename)
           end
           
+          # Clean the file from the filesystem
           def destroy_attached_file
             attached_file.destroy
           end
       end
     end
 
+    # AttachableFile is what Milton uses to interface between your model and
+    # the underlying file. Rather than just pushing a whole bunch of methods
+    # into your model, you get a reference to an AttachableFile (or something
+    # that extends AttachableFile).
     class AttachableFile
       class_inheritable_accessor :options
 
@@ -84,7 +100,7 @@ module Citrusbyte
         # Scheme allows for 1000 billion files while never storing more than
         # 1000 files in a single folder.
         #
-        # Can overwrite this method to provide your own partioning scheme.
+        # Can overwrite this method to provide your own partitioning scheme.
         def partition(id)
           # TODO: there's probably some fancy 1-line way to do this...
           padded = ("0"*(12-id.to_s.size)+id.to_s).split('')
@@ -92,62 +108,65 @@ module Citrusbyte
         end
       end
       
+      # TODO: can probably fanagle a way to only pass a reference to the model
+      # and not need the filename (or better yet just the filename and 
+      # decouple)
       def initialize(attachment, filename)
         @attachment = attachment
         @filename   = filename
       end
 
-      # Returns the full path and filename to the file with the given options
+      # Returns the full path and filename to the file with the given options.
       # If no options are given then returns the path and filename to the
-      # original file
+      # original file.
       def path(options={})
-        options.empty? ? File.join(dirname, @filename) : DerivativeFile.new(@filename, options).path
+        options.empty? ? File.join(dirname, @filename) : Derivative.new(@filename, options).path
       end
       
-      # Returns the path to the file from the public root (assuming it's
-      # stored somewhere under the public root)
-      def public_path(options={})
-        path(options).gsub(/.*public/, '')
-      end
-      
+      # Returns the full directory path up to the file, w/o the filename.
       def dirname
         File.join(root_path, partitioned_path)
       end
       
-      # Returns true if the source file associated w/ the given model exists,
-      # otherwise returns false
+      # Returns true if the file exists on the underlying file system.
       def exists?
         File.exist?(path)
       end
       
-      # Removes the file associated w/ this attachment from disk
+      # Removes the file from the underlying file system and any derivatives of
+      # the file.
       def destroy
         destroy_derivatives
         destroy_file
       end
       
       protected
-        # Returns the file as a File object
+        # Returns the file as a File object opened for reading.
         def file_reference
-          @file_reference ||= File.new(path)
-          @file_reference
+          File.new(path)
         end
         
+        # Returns the partitioned path segment based on the id of the model
+        # this file is attached to.
         def partitioned_path
           self.class.partition(@attachment.id)
         end
 
-        # The full path to the root of where files will be stored on disk
+        # The full path to the root of where files will be stored on disk.
         def root_path
           self.class.options[:file_system_path]
         end
 
-        # Recreates the directory this file will be stored in, as well as the
-        # path for any derivatives of this file
+        # Recreates the directory this file will be stored in.
         def recreate_directory
           self.class.recreate_directory(dirname) unless File.exists?(dirname)
         end
         
+        # Removes the file from the filesystem.
+        def destroy_file
+          FileUtils.rm path if File.exists?(path)
+        end
+
         # Derivatives of this Attachment ====================================
         
         # Returns an array of derivatives of this attachment
@@ -157,24 +176,29 @@ module Citrusbyte
           end
         end
         
-        # Recreates the directory this file will be stored in, as well as the
-        # path for any derivatives of this file
+        # Recreates the directory derivatives of this file will be stored in.
         def recreate_derivative_directory
           dirname = Derivative.dirname_for(path)
           self.class.recreate_directory(dirname) unless File.exists?(dirname)
         end
 
-        # Removes the derivatives folder for this file and all files within
+        # Removes the derivatives folder for this file and all files within.
         def destroy_derivatives
           FileUtils.rm_rf dirname if File.exists?(dirname)
         end
-        
-        # Removes the file from the filesystem
-        def destroy_file
-          FileUtils.rm path if File.exists?(path)
-        end
     end
-    
+
+    # Represents a file created on the file system that is a derivative of the
+    # one referenced by the model, i.e. a thumbnail of an image, or a transcode
+    # of a video.
+    # 
+    # Provides a container for options and a uniform API to dealing with
+    # passing options for the creation of derivatives.
+    # 
+    # Files created as derivatives have their creation options appended into
+    # their filenames so it can be checked later if a file w/ the given
+    # options already exists (so as not to create it again).
+    # 
     class Derivative
       attr_reader :options
       
@@ -196,10 +220,14 @@ module Citrusbyte
           File.basename(filename, File.extname(filename)) + (append.blank? ? '' : "#{AttachableFile.options[:separator]}#{append}") + File.extname(filename)
         end
         
+        # Given a filename (presumably with options embedded in it) parses out
+        # the options and returns them as a hash.
         def extract_options_from(filename)
           File.basename(filename, File.extname(filename))[(filename.rindex(AttachableFile.options[:separator]) + 1)..-1]
         end
         
+        # Creates a new Derivative from the given filename by extracting the
+        # options.
         def from_filename(filename)
           Derivative.new(filename, options_from(extract_options_from(filename)))
         end
@@ -218,14 +246,17 @@ module Citrusbyte
         @options = options
       end
       
+      # The filename of this Derivative with embedded options.
       def filename
         self.class.filename_for(@file.path, options)
       end
       
+      # The full path and filename to this Derivative.
       def path
         File.join(Derivative.dirname_for(@file.path), filename)
       end
       
+      # Returns true if the file resulting from this Derivative exists.
       def exists?
         File.exists?(path)
       end
