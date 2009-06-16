182e1a7b89fc45cc5f1919070f19a5b1d9ce97e5
benalavi <benalavi@gmail.com>
2009-06-16 01:34:22 -0700

    add s3 file expectations

--- test/milton/storage/s3_file_test.rb
+++ test/milton/storage/s3_file_test.rb
@@ -5,41 +5,42 @@ require 'milton/storage/s3_file'
 module Milton
   class S3FileTest < ActiveSupport::TestCase
     setup do
-      @key    = flexmock(RightAws::S3::Key,
-        :exists? => true,
-        :delete  => true
-      )
-      @bucket = flexmock(RightAws::S3::Bucket,
-        :put => true,
-        :key => @key
-      )
-      @file   = Storage::S3File.new('milton.jpg', 1, :storage_options => {
+      @file = Storage::S3File.new('milton.jpg', 1, :storage_options => {
         :bucket      => 'milton',
         :permissions => 'private',
         :protocol    => 'https'
-      })
-  
-      flexmock(@file).should_receive(:bucket).and_return(@bucket)
+      })  
     end
 
     context "storing a file" do
-      pending_test "store the file with the given permissions" do
+      should "store the file with the given permissions, using id and filename as key" do
+        flexmock(@file).should_receive(:bucket).and_return(@bucket = flexmock("Fake S3 Bucket"))
+        @bucket.should_receive(:put).with('1/milton.jpg', any(), any(), 'private').once.and_return(true)
+        
+        @file.store(fixture_file('mini-milton.jpg'))
       end
   
-      pending_test "store the file using the given protocol" do
-      end
-  
-      pending_test "combine the id and filename as the pathname of the file" do
+      should "store the file using the given protocol" do
+        flexmock(RightAws::S3).should_receive(:new).once.with(any(), any(), { :protocol => 'https', :port => 443, :logger => Rails.logger }).and_return(@s3 = flexmock("Fake S3"))
+        flexmock(@s3, :bucket => flexmock("Fake S3 Bucket", :put => true))
+        
+        @file.store(fixture_file('mini-milton.jpg'))
       end
     end
 
     context "retrieving a stored file" do
-      pending_test "retrive the file in the given bucket w/ id and filename" do
+      should "retrive the file in the given bucket w/ id and filename" do
+        assert_equal "http://milton.s3.amazonaws.com/1/milton.jpg", @file.path
       end
     end
 
     context "destroying a file" do
-      pending_test "delete the file w/ id and filename" do
+      should "delete the file w/ id and filename" do
+        flexmock(@file, :bucket => @bucket = flexmock("Fake S3 Bucket"))
+        flexmock(@bucket).should_receive(:key).with('1/milton.jpg').once.and_return(@key = flexmock("Fake S3 Key"))
+        flexmock(@key).should_receive(:delete).once.and_return(true)
+        
+        @file.destroy
       end
     end
   end
